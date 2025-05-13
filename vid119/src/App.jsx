61d8d98eeeaab7b56e9cc1ba8311a6893bc8ffc7
Vivek0925 <vivekmandal123v@gmail.com>
2025-05-13 16:02:08 +0530

    done

---
+++ vid119/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import { useForm } from "react-hook-form"
+
+function App() {
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors , isSubmitting },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data)
+  return (
+    <>
+    <div className="container">
+
+       <form onSubmit={handleSubmit(onSubmit)}>
+
+          <input placeholder='username' {...register("username", { required: true , minLength: {value: 3, message: "this should be min 3 letters"},maxLength :{value : 8 , message: "min 8 letters"} })} type='text' />
+          {errors.username && <span>{errors.username.message}</span>}
+<br />
+          <input placeholder='password'{...register("password", { required: true, minLength: { value: 3, message: "this should be min 3 letters" }, maxLength: { value: 8, message: "min 8 letters"} })} type='text' />
+          {errors.password && <span>{errors.password.message}</span>}
+<br />
+          <input disabled={isSubmitting} type="submit" value="submit" />
+      </form>
+    </div>
+      
+    </>
+  )
+}
+
+export default App

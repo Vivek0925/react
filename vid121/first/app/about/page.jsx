467c9c7b0f05d194b362c0b46e2760e6b6b10884
Vivek0925 <vivekmandal123v@gmail.com>
2025-10-21 16:22:15 +0530

    next intro

---
+++ vid121/first/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>i m about</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ vid121/first/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div>
+  <h1>i am next app</h1>
+</div>
+  );
+}

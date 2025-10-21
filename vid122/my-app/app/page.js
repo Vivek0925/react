12d446f3e90c381b45282ece2717aa14d28ef1b3
Vivek0925 <vivekmandal123v@gmail.com>
2025-10-21 21:08:28 +0530

    done

--- vid122/my-app/app/page.js
+++ vid122/my-app/app/page.js
@@ -1,11 +1,16 @@
-"use client"  
-import { useState, useEffect } from "react";
+
+// import { useState, useEffect } from "react";
+import fs from 'fs/promises'
+import { to } from './../../../vid121/first/.next/static/chunks/50986_next_dist_client_6fa53630._';
 export default function Home() {
-  const [count, setcount] = useState(0)
+  // const [count, setcount] = useState(0)
+  console.log("rendered 2")
+  let a  = fs.readFile(".gitignore")
+  a.then(e=> {console.log(e.toString())})
   return (
    <div>
-    i am component based app{count}
-    <button className="bg-amber-300" onClick={()=> setcount(count + 1)}>hello</button>
+    i am component based app
+    {/* <button className="bg-amber-300" onClick={()=> setcount(count + 1)}>hello</button> */}
    </div>
   );
 }

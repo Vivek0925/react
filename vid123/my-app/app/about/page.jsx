701465abaad735238d60439e5eb548e0fb0db118
Vivek0925 <vivekmandal123v@gmail.com>
2025-10-22 15:56:34 +0530

    links

---
+++ vid123/my-app/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a company that values excellence and innovation.</p>
+    </div>
+  )
+}
+
+export default page
---
+++ vid123/my-app/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-[87vh] items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>i am component</h1>
+    </div>
+  );
+}

21b1a6b1b463ee5e628eecc27316493df2ef4026
Vivek0925 <vivekmandal123v@gmail.com>
2025-10-22 16:22:07 +0530

    images

--- vid123/my-app/app/page.js
+++ vid123/my-app/app/page.js
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="flex min-h-[87vh] items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>i am component</h1>
+      //image tag
+      <Image src="https://www.bigfootdigital.co.uk/wp-content/uploads/2020/07/image-optimisation-scaled.jpg" alt="Description of image" width={500} height={500} />
+      
     </div>
   );
 }
--- vid123/my-app/next.config.mjs
+++ vid123/my-app/next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "",
+        hostname: "www.bigfootdigital.co.uk",
+        port: "",
+        pathname: "",
+        search: "",
+      },
+    ],
+  },
   /* config options here */
   reactCompiler: true,
 };
